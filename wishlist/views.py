db7439b75bb6dd27c969e6cfdc054dd641e32afb
BalqisAsySyifaLumbun <balqislumbun@gmail.com>
2022-09-22 09:06:50 +0700

    22/09

--- wishlist/views.py
+++ wishlist/views.py
@@ -2,12 +2,24 @@ from django.http import HttpResponse
 from django.core import serializers
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+@login_required(login_url='/wishlist/login/')
 def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
-    'list_barang': data_barang_wishlist,
-    'nama': 'Balqis Lumbun -OKA'
+    'wishlists': data_barang_wishlist,
+    'nama': 'Balqis Lumbun -OKA',
+    'npm' : '2106751184',
+    'last_login': request.COOKIES['last_login'],
+
     }
     return render(request, "wishlist.html", context)
 def show_xml(request):
@@ -19,9 +31,39 @@ def show_json(request):
 def show_json_by_id(request,id):
     data = BarangWishlist.objects.filter(pk=id)
     # Jika JSON
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
-    
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")   
 def show_xml_by_id(request,id):
     data = BarangWishlist.objects.filter(pk=id)
     # Jika XML
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def register(request):
+        form = UserCreationForm()   
+        if request.method == "POST":
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Akun telah berhasil dibuat!')
+                return redirect('wishlist:login')
+        
+        context = {'form':form}
+        return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("wishlist:show_wishlist")) # membuat response
+            response.set_cookie('last_login', str(datetime.datetime.now())) # membuat cookie last_login dan menambahkannya ke dalam response
+            return response
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('wishlist:login')
